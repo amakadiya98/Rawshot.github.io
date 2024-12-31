866d0d7ebdbd8e0a2180e1b597745c6a76430b2c
amakadiya98 <amakadiya98@gmail.com>
2024-12-31 18:58:56 +0530

    initial commit for landing page

---
+++ assets/js/index.js
@@ -0,0 +1,80 @@
+
+$(function () {
+    $(".video-carousel").slick({
+        infinite: true,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 2000,
+        cssEase: "linear",
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
+
+var $st = $('.pagination');
+    var $slickEl = $('.center');
+
+    $slickEl.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $st.text(i + ' of ' + slick.slideCount);
+    });
+
+    $slickEl.slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        focusOnSelect: true,
+        dots: false,
+        infinite: true,
+        arrows: false,
+        responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2, 
+                centerMode: true, 
+                centerPadding: '10px',
+            }
+        },
+        {
+            breakpoint: 470,
+            settings: {
+                slidesToShow: 2, 
+                centerMode: false,
+                centerPadding: '10px',
+            }
+        }
+    ]
+    });
